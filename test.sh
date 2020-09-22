6f7e50dc6aa7aad5a70b0d9ed704cd99af3bdca7
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-21 22:20:48 -0700

    Fix test.sh

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@ JULIA="$PWD/usr/bin/julia"
 set -ex
 make clean
 make -j2
-cd ~/.julia/dev/PyCall
 "$JULIA" --startup-file=no -e 'using Pkg; pkg"add PackageCompiler#cb994c72e2087c57ffa4727ef93589e1b98d8a32"'
 "$JULIA" --startup-file=no -e 'using Pkg; pkg"dev PyCall'
+cd ~/.julia/dev/PyCall
 "$JULIA" --startup-file=no aot/compile.jl
 aot/julia.sh --startup-file=no -e 'using InteractiveUtils; versioninfo()'
