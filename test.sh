48353c0acf67f2b61a2801febe03fb5828ba9777
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-21 20:10:44 -0700

    Use make -j2

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 JULIA="$PWD/usr/bin/julia"
 set -ex
 make clean
-make
+make -j2
 cd ~/.julia/dev/PyCall
 "$JULIA" --startup-file=no aot/compile.jl
 aot/julia.sh --startup-file=no -e 'using InteractiveUtils; versioninfo()'
